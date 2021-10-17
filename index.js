6d81a1fca10759db9f7931ff444e224f879fc4e2
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-10-17 11:52:30 +0530

    Added logic to generate random num.

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+let randomNumber;
+
+const generateRandomNumber = () => {
+  // Generated any random number from 0 to 1
+  let random = Math.random();
+
+  randomNumber = Math.floor(random * 6 + 1);
+};
+
 function blink_text() {
   $(".note").fadeOut(500);
   $(".note").fadeIn(500);
 }
+
+$(".generate-button").on("click", () => {
+  generateRandomNumber();
+  console.log(randomNumber);
+});
 
 setInterval(blink_text, 1000);
